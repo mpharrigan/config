857bff7a670468de5f6faa650c946fea847878fe
Matthew Harrigan <harrigan@stanford.edu>
2015-07-21 12:54:25 -0700

    ml-tux

---
+++ _bash_profiles/mltux.sh
@@ -0,0 +1,7 @@
+export PATH=$HOME/opt/python/bin:$PATH
+export PATH=$HOME/opt/node/bin:$PATH
+export PATH=$HOME/opt/pycharm/bin:$PATH
+
+# Find my QT
+export CMAKE_PREFIX_PATH=$HOME/opt/Qt5.4.0/5.4/gcc_64/
+
