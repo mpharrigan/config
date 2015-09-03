d4653da7caab5ca2d1eaae3f961d64d10c6e4f8b
Matthew Harrigan <harrigan@stanford.edu>
2015-09-02 19:46:17 -0700

    New mltux

--- _bash_profiles/mltux.sh
+++ _bash_profiles/mltux.sh
@@ -1,7 +1,2 @@
-export PATH=$HOME/opt/python/bin:$PATH
-export PATH=$HOME/opt/node/bin:$PATH
-export PATH=$HOME/opt/pycharm/bin:$PATH
-
-# Find my QT
-export CMAKE_PREFIX_PATH=$HOME/opt/Qt5.4.0/5.4/gcc_64/
+export PATH=$HOME/opt/miniconda3/bin:$PATH
 
