be0576cf1f3e50d7d02cab49d6c1cb038f6d4a4c
Matthew Harrigan <harrigan@stanford.edu>
2015-11-23 12:57:56 -0800

    Install amber

--- profiles/vspm58.sh
+++ profiles/vspm58.sh
@@ -1,2 +1,7 @@
 export PATH="$HOME/opt/miniconda3/bin:$PATH"
 export PATH="$HOME/opt/vmd/bin:$PATH"
+
+
+export AMBERHOME="$HOME/opt/amber14"
+export PATH="$AMBERHOME/bin:$PATH"
+export LD_LIBRARY_PATH="$AMBERHOME/lib:$LD_LIBRARY_PATH"
