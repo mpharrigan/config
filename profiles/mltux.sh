628555681e9fd010661e7f9b09218877a1551951
Matthew Harrigan <harrigan@stanford.edu>
2015-12-13 12:24:25 -0800

    Install amd app sdk

--- profiles/mltux.sh
+++ profiles/mltux.sh
@@ -1,2 +1,6 @@
 export PATH="$HOME/opt/miniconda3/bin:$PATH"
 export PATH="$HOME/opt/node/bin:$PATH"
+
+export AMDAPPSDKROOT="$HOME/opt/AMDAPPSDK-2.9-1"
+export OPENCL_VENDOR_PATH="$HOME/opt/AMDAPPSDK-2.9-1/etc/OpenCL/vendors/"
+export LD_LIBRARY_PATH="$HOME/opt/AMDAPPSDK-2.9-1/lib/x86_64/:$LD_LIBRARY_PATH"
