710c8b628ef3445e7f7e37ce4d40e361cee515e7
Matthew Harrigan <harrigan@stanford.edu>
2015-12-21 10:41:20 -0800

    Update sherlock software

--- profiles/sher.sh
+++ profiles/sher.sh
@@ -1,8 +1,8 @@
 PATH=$HOME/bin:$PATH
-PATH=$HOME/opt/python/bin:$PATH
+PATH=$HOME/opt/miniconda3/bin:$PATH
 PATH=$HOME/opt/git/bin:$PATH
-PATH=$HOME/opt/cmake/bin:$PATH
 PATH=$HOME/opt/gromacs/bin:$PATH
 export PATH
 
 module load gcc
+module load cmake
