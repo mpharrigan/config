474fdbf761e8ed01ed6c7c5fc325bc5067649df1
Matthew Harrigan <harrigan@stanford.edu>
2016-02-04 15:59:48 -0800

    Add pycharm to path

--- profiles/vspm58.sh
+++ profiles/vspm58.sh
@@ -1,6 +1,7 @@
 export PATH="$HOME/opt/miniconda3/bin:$PATH"
 export PATH="$HOME/opt/vmd/bin:$PATH"
 export PATH="$HOME/opt/gromacs/bin:$PATH"
+export PATH="$HOME/opt/pycharm/bin:$PATH"
 export PATH="$HOME/.cabal/bin:$PATH"
 
 # Amber
