6da81a2c838eda4414714c2fed93300a2a3707ff
Matthew Harrigan <harrigan@stanford.edu>
2016-03-19 18:01:00 -0700

    Install vmd

--- profiles/mltux.sh
+++ profiles/mltux.sh
@@ -1,6 +1,7 @@
 export PATH="$HOME/opt/miniconda3/bin:$PATH"
 export PATH="$HOME/opt/node/bin:$PATH"
 export PATH="$HOME/opt/uncrustify/bin:$PATH"
+export PATH="$HOME/opt/vmd/bin:$PATH"
 
 export AMDAPPSDKROOT="$HOME/opt/AMDAPPSDK-2.9-1"
 export OPENCL_VENDOR_PATH="$HOME/opt/AMDAPPSDK-2.9-1/etc/OpenCL/vendors/"
