929f047a218e903edfb3a9d8b3a8247bda7f2f01
Matthew Harrigan <harrigan@stanford.edu>
2016-08-10 12:08:26 -0700

    Install cabal

--- profiles/ml-tux.sh
+++ profiles/ml-tux.sh
@@ -1,9 +1,6 @@
 export PATH="$HOME/bin:$PATH"
-#export PATH="$HOME/opt/miniconda3/bin:$PATH"
-#export PATH="$HOME/opt/node/bin:$PATH"
-#export PATH="$HOME/opt/uncrustify/bin:$PATH"
-#export PATH="$HOME/opt/vmd/bin:$PATH"
-#
+export PATH="$HOME/.cabal/bin:$PATH"
+
 #export AMDAPPSDKROOT="$HOME/opt/AMDAPPSDK-2.9-1"
 #export OPENCL_VENDOR_PATH="$HOME/opt/AMDAPPSDK-2.9-1/etc/OpenCL/vendors/"
 #export LD_LIBRARY_PATH="$HOME/opt/AMDAPPSDK-2.9-1/lib/x86_64/:$LD_LIBRARY_PATH"
