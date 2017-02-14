85ad74313d2099a774d332ab95b3e9186eb33691
Matthew Harrigan <harrigan@stanford.edu>
2017-02-14 00:37:23 -0800

    Use sherlock git

--- profiles/sher.sh
+++ profiles/sher.sh
@@ -1,6 +1,5 @@
 PATH=$HOME/bin:$PATH
 PATH=$HOME/opt/miniconda3/bin:$PATH
-PATH=$HOME/opt/git/bin:$PATH
 PATH=$HOME/opt/gromacs/bin:$PATH
 export PATH
 
@@ -11,4 +10,5 @@ export LD_LIBRARY_PATH="$AMBERHOME/lib:$LD_LIBRARY_PATH"
 module load cmake/3.2.2
 module load gcc/5.3.0
 module load openmpi/2.0.0
+module load git
 
