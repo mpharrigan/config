1ab1b3bab09102e2862c21677a3a40358bd24927
Matthew Harrigan <harrigan@stanford.edu>
2017-02-14 00:32:29 -0800

    amber and versioned modules

--- profiles/sher.sh
+++ profiles/sher.sh
@@ -4,8 +4,11 @@ PATH=$HOME/opt/git/bin:$PATH
 PATH=$HOME/opt/gromacs/bin:$PATH
 export PATH
 
-module load gcc
-module load cmake
-module load jre
-module load scala
+export AMBERHOME="$HOME/opt/amber16"
+export PATH="$AMBERHOME/bin:$PATH"
+export LD_LIBRARY_PATH="$AMBERHOME/lib:$LD_LIBRARY_PATH"
+
+module load cmake/3.2.2
+module load gcc/5.3.0
+module load openmpi/2.0.0
 
