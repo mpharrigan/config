8d8afa45aa46fd432063300602a7f028a20d4c2a
Matthew Harrigan <harrigan@stanford.edu>
2017-03-25 14:52:54 -0700

    Sherlock 2

--- profiles/sher.sh
+++ profiles/sher.sh
@@ -6,9 +6,3 @@ export PATH
 export AMBERHOME="$HOME/opt/amber16"
 export PATH="$AMBERHOME/bin:$PATH"
 export LD_LIBRARY_PATH="$AMBERHOME/lib:$LD_LIBRARY_PATH"
-
-module load cmake/3.2.2
-module load gcc/5.3.0
-module load openmpi/2.0.0
-module load git
-
