df28dc843330678737c29567e3c37896729236e3
Matthew Harrigan <harrigan@stanford.edu>
2017-03-25 16:55:06 -0700

    Sherlock 2

--- profiles/sher.sh
+++ profiles/sher.sh
@@ -1,8 +1,7 @@
-PATH=$HOME/bin:$PATH
-PATH=$HOME/opt/miniconda3/bin:$PATH
-PATH=$HOME/opt/gromacs/bin:$PATH
-export PATH
+# Slurm-status
+export PATH="$HOME/source/slurm-status:$PATH"
 
+# Amber
 export AMBERHOME="$HOME/opt/amber16"
 export PATH="$AMBERHOME/bin:$PATH"
 export LD_LIBRARY_PATH="$AMBERHOME/lib:$LD_LIBRARY_PATH"
