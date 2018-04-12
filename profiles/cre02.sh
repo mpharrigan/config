e13c0d9a41fca3d40cb4be0c5d1f3edf55b786e2
Matthew Harrigan <matt.harrigan@rigetti.com>
2018-04-12 12:45:51 -0700

    new conda activate

--- profiles/cre02.sh
+++ profiles/cre02.sh
@@ -1,4 +1,5 @@
-export PATH="$HOME/miniconda3/bin:$PATH"
 export PATH="$HOME/opt/sbcl/bin:$PATH"
 export SBCL_HOME="$HOME/opt/sbcl/lib/sbcl"
 export LD_LIBRARY_PATH="$HOME/opt/expokit:$LD_LIBRARY_PATH"
+
+. "$HOME/miniconda3/etc/profile.d/conda.sh"
