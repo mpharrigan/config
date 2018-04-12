f8f510c091269e0ee0b0dcdcb9d8be307ddc968e
Matthew Harrigan <matt.harrigan@rigetti.com>
2018-04-12 12:43:56 -0700

    cre02

---
+++ profiles/cre02.sh
@@ -0,0 +1,4 @@
+export PATH="$HOME/miniconda3/bin:$PATH"
+export PATH="$HOME/opt/sbcl/bin:$PATH"
+export SBCL_HOME="$HOME/opt/sbcl/lib/sbcl"
+export LD_LIBRARY_PATH="$HOME/opt/expokit:$LD_LIBRARY_PATH"
